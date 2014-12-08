2d7a079f1d61f13998fecac62ba62e01321ffc4a
Yehor Lvivski <lvivski@gmail.com>
2014-12-08 19:35:13 +0200

    fixes

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 var browserify = require('browserify'),
 
-    rework = require('rework'),
-    reworkNpm = require('rework-npm'),
-    prefixes = require('autoprefixer')().rework,
-    vars = require('rework-vars')(),
-    calc = require('rework-calc'),
-    color = require('rework-color-function'),
+	rework = require('rework'),
+	reworkNpm = require('rework-npm'),
+	prefixes = require('autoprefixer'),
+	inherit = require('revork-inherit'),
+	vars = require('rework-vars'),
+	calc = require('rework-calc'),
+	color = require('rework-color-function'),
 
-    fs = require('fs'),
-    path = require('path'),
-    Davy = require('davy')
+	fs = require('fs'),
+	path = require('path'),
+	Davy = require('davy')
 
 exports.js = function (options) {
 	options = options || {}
@@ -29,10 +30,19 @@ exports.css = function (options) {
 	return Davy.wrap(fs.readFile)(options.entry, 'utf8').then(function (css) {
 		return rework(css)
 			.use(reworkNpm(path.dirname(options.entry)))
-			.use(vars)
+			.use(vars())
 			.use(calc)
 			.use(color)
-			.use(prefixes)
+			.use(inherit())
+			.use(prefixes().rework)
 			.toString()
+	})
+}
+
+exports.files = function (options) {
+	options = options || {}
+
+	options.files.forEach(function (entry) {
+		fs.createReadStream(entry).pipe(fs.createWriteStream(entry));
 	})
 }
