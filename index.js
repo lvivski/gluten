75489b9d9d95749d33b1cdca034ec5fe3a8be5e1
Yehor Lvivski <lvivski@gmail.com>
2015-08-21 20:57:23 +0300

    better, new

--- index.js
+++ index.js
@@ -1,48 +1,126 @@
-var browserify = require('browserify'),
+require('es6-promise').polyfill();
 
-	rework = require('rework'),
-	reworkNpm = require('rework-npm'),
-	prefixes = require('autoprefixer'),
-	inherit = require('revork-inherit'),
-	vars = require('rework-vars'),
-	calc = require('rework-calc'),
-	color = require('rework-color-function'),
-
-	fs = require('fs'),
+var fs = require('fs'),
 	path = require('path'),
-	Davy = require('davy')
+
+	browserify = require('browserify'),
+	watchify = require('watchify'),
+	babelify = require('babelify'),
+	envify = require('envify'),
+	browserifyShim = require('browserify-shim'),
+
+	postcss = require('postcss'),
+	chokidar = require('chokidar'),
+	autoprefixer = require('autoprefixer'),
+	postcssCalc = require('postcss-calc'),
+	postcssCustomProperties = require('postcss-custom-properties'),
+	postcssImport = require('postcss-import'),
+	postcssNested = require('postcss-nested'),
+	postcssUrl = require('postcss-url');
 
 exports.js = function (options) {
-	options = options || {}
+	options = options || {};
 
-	var b = browserify({
-		basedir: options.basedir
-	})
+	if (process.env.NODE_ENV === 'development') {
+		options.debug = true;
+	}
+	if (options.watch) {
+		assign(options, watchify.args);
+	}
 
-	b.require('./' + options.entry, {entry: true})
+	var bundler = browserify(options);
 
-	return Davy.wrap(b.bundle.bind(b))({debug: false})
-}
+	if (options.watch) {
+		var watcher = watchify(bundler);
+		watcher.on('update', function () {
+			watcher
+				.bundle(function () {
+					console.log('js changed');
+				})
+				.pipe(fs.createWriteStream(path.resolve(process.cwd(), options.output)))
+		})
+	}
+
+	bundler
+		.add(path.resolve(process.cwd(), options.entry))
+		.transform(babelify.configure({
+			loose: ['es6.modules'],
+			sourceMapRelative: '.'
+		}));
+
+	if (options.shim) {
+		bundler.transform(browserifyShim);
+	}
+
+	bundler
+		.transform(envify)
+		.bundle(function () {
+			console.log('js build');
+		})
+		.pipe(fs.createWriteStream(path.resolve(process.cwd(), options.output)));
+};
 
 exports.css = function (options) {
-	options = options || {}
+	options = options || {};
 
-	return Davy.wrap(fs.readFile)(options.entry, 'utf8').then(function (css) {
-		return rework(css)
-			.use(reworkNpm(path.dirname(options.entry)))
-			.use(vars())
-			.use(calc)
-			.use(color)
-			.use(inherit())
-			.use(prefixes().rework)
-			.toString()
-	})
+	var watchedFiles = [],
+		postcssWatch = function () {};
+
+	if (options.watch) {
+		var watcher = chokidar.watch(watchedFiles);
+
+		watcher.on('change', function () {
+			console.log('css changed');
+			processFiles(processor, options);
+		});
+
+		postcssWatch = function (files) {
+			watcher.unwatch(watchedFiles);
+			watcher.add(files);
+			watchedFiles = files;
+		}
+	}
+
+	var processor = postcss([
+			postcssImport({
+				onImport: postcssWatch
+			}),
+			postcssNested(),
+			postcssCustomProperties(),
+			postcssCalc(),
+			autoprefixer({
+				browsers: [
+					'> 1%',
+					'IE 8'
+				]
+			})
+		])
+		.use(postcssUrl({
+			url: 'copy',
+			useHash: true,
+			assetsPath: 'assets'
+		}));
+
+	processFiles(processor, options);
+};
+
+function processFiles(processor, options) {
+	var css = fs.readFileSync(options.entry, "utf8");
+
+	processor
+		.process(css, {from: options.entry, to: options.output})
+		.then(function (result) {
+			fs.writeFileSync(options.output, result.css);
+		});
 }
 
-exports.files = function (options) {
-	options = options || {}
-
-	options.files.forEach(function (entry) {
-		fs.createReadStream(entry).pipe(fs.createWriteStream(entry));
-	})
+function assign(obj) {
+	var i = 1;
+	while (i < arguments.length) {
+		var source = arguments[i++];
+		for (var property in source) if (source.hasOwnProperty(property)) {
+			obj[property] = source[property];
+		}
+	}
+	return obj;
 }
